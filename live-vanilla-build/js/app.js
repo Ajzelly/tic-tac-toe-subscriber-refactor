8dbc3c36682841c9cf7b76718e7eef83932a7559
Ajzelly <mohamed.abdulljaber@strathmore.edu>
2024-01-30 11:27:45 -0800

    Completed

---
+++ live-vanilla-build/js/app.js
@@ -0,0 +1,61 @@
+import Store from "./store.js";
+import View from "./view.js";
+
+const players = [
+  {
+    id: 1,
+    name: "Player 1",
+    iconClass: "fa-x",
+    colorClass: "turquoise",
+  },
+  {
+    id: 2,
+    name: "Player 2",
+    iconClass: "fa-o",
+    colorClass: "yellow",
+  },
+];
+
+function init() {
+  const view = new View(); // Use lowercase 'view' instead of 'View'
+  const store = new Store("live=t3-storage-key", players); // Use lowercase 'store' instead of 'Store'
+
+  // Current tab state changes
+  store.addEventListener("Statechange", () => {
+    view.render(store.game, store.stats);
+  });
+
+  // A different tab state changes
+  window.addEventListener('storage', () => {
+    console.log("State changed from another tab");
+    view.render(store.game, store.stats);
+  });
+
+  // The first load of the document
+  view.render(store.game, store.stats);
+
+  view.bindGameResetEvent((event) => {
+    store.reset();
+  });
+
+  view.bindNewRoundEvent((event) => {
+    store.newRound();
+  });
+
+  view.bindPlayerMoveEvent((square) => {
+    console.log(square);
+
+    const existingMove = store.game.moves.find(
+      (move) => move.squareId === +square.id
+    );
+
+    if (existingMove) {
+      return;
+    }
+
+    // Advance to the next state by pushing a move to the moves array
+    store.playerMove(+square.id);
+  });
+}
+
+window.addEventListener("load", init);
